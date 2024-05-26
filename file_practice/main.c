8468c134b4d40340b83ce28eb983c059b5f76bfa
CallumQiu <m11015008@gapps.ntust.edu.tw>
2024-05-26 18:29:48 +0800

    add read function

--- file_practice/main.c
+++ file_practice/main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 int main(){
-
+    /* read file */
+    FILE *fptr;
+    fptr = fopen("./data.txt", "r");
+    char buffer[1024];
+    fread(buffer, 1024, 1, fptr);
+    printf("%s \n", buffer);
+    fclose(fptr);
     return 0;
 }
