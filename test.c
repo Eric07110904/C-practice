3a3bd912de45bcd8b47a6eedb3057c9a4ae596fe
CallumQiu <m11015008@gapps.ntust.edu.tw>
2024-05-27 16:17:14 +0800

    update test.c

--- test.c
+++ test.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>  
 
+void bark_b(){
+    printf("bark bark bark b b b \n");
+}
+
+void bark_c(){
+    printf("bark bark bark c c c \n ");
+}
+
 int main(){
 
     printf("hello world\n");
