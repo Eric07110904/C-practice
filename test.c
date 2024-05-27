908964343e5d4964b0dd9f330f4abdf6ec01eaea
CallumQiu <m11015008@gapps.ntust.edu.tw>
2024-05-27 16:24:07 +0800

    add funct2

--- test.c
+++ test.c
@@ -4,10 +4,13 @@
 void bark_A(){
     printf("bark bark bark A A A ");
 }
+void bark_B(){
+    printf("bark bark bark B B B ");
+}
 int main(){
 
     printf("hello world\n");
-
+    printf("hello world2\n");
 
 
     return 0;
